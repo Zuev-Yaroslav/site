87f615933df1aa133e13f8b612a61e0e553ba2ba
Zuev-Yaroslav <zuevyaroslav937@gmail.com>
2022-10-16 14:34:35 +0300

    FIRST

--- git.bat
+++ git.bat
@@ -1,7 +1,7 @@
 cd C:\Users\zuevy\OneDrive\Рабочий стол\site
 C:\Git\bin\git.exe init
 C:\Git\bin\git.exe add .
-C:\Git\bin\git.exe commit -m "FIRST COMMIT"
+C:\Git\bin\git.exe commit -m "FIRST"
 C:\Git\bin\git.exe remote -v
 C:\Git\bin\git.exe remote add origin https://github.com/Zuev-Yaroslav/site.git
 C:\Git\bin\git.exe push origin master
